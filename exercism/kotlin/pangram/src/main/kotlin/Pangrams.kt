42a4c5176edb39ec2b6de5cf7de0fa5631ffbb3c
Muhammad Lukman Low <kianseong@gmail.com>
2018-02-06 06:20:16 +0800

    simplified Pangrams

--- exercism/kotlin/pangram/src/main/kotlin/Pangrams.kt
+++ exercism/kotlin/pangram/src/main/kotlin/Pangrams.kt
@@ -1,11 +1,8 @@
 object Pangram {
 	fun isPangram(input: String): Boolean {
-		val re = Regex("[^A-Za-z]")
-		// return if (re.replace(input, "").distinct().size == 26) true else false
-		var length = re.replace(input, "")
-			     .toLowerCase()
-			     .toCharArray().distinct().size
-		if (length == 26) { 
-			return true } else { return false }
+		return (Regex("[^A-Za-z]").replace(input, "")
+		        .toLowerCase()
+			.toCharArray()
+			.distinct().size == 26)
 	}
 }
