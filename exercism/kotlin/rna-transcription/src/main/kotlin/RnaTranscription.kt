38b7ad9de9e0ce4c0f8a844c5aab26801790ed46
Low Kian Seong <lowks@users.noreply.github.com>
2018-02-06 12:10:30 +0800

    Update RnaTranscription.kt
    Betterment of the code.

--- exercism/kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ exercism/kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,17 +1,8 @@
-fun returnmapping(char: String): String {
-	var return_string = when(char) {
-		"G" -> "C"
-		"C" -> "G"
-		"T" -> "A"
-		"A" -> "U"
-		"U" -> "A"
-		else -> "nothing"
+object Pangram {
+	fun isPangram(input: String): Boolean {
+		return (Regex("[^A-Za-z]").replace(input, "")
+		        .toLowerCase()
+			.toCharArray()
+			.distinct().size == 26)
 	}
-	return return_string 
-}
-
-fun transcribeToRna(dna: String): String {
-	return if (dna.length == 1) returnmapping(dna) else {  dna.toCharArray()
-						       .map{ returnmapping(it.toString()) }
-						       .joinToString("") }
 }
