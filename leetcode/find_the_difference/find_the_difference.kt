87d18498018ceadaffdb05c0056b18feefe562ac
Muhammad Lukman Low <kianseong@gmail.com>
2018-02-20 21:22:46 +0800

    leetcode add find the difference

---
+++ leetcode/find_the_difference/find_the_difference.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun findTheDifference(s: String, t: String): Char {    
+        var s1 = s.toCharArray()
+        var s2 = t.toCharArray().toMutableList()
+        return s2[0]
+    }
+}
