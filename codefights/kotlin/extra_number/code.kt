e7a8491095d6d7ba535bf6e64eee0e603783aa35
Muhammad Lukman Low <kianseong@gmail.com>
2018-02-21 18:03:09 +0800

    Adding alternate solution to extra number to kotlin solution

--- codefights/kotlin/extra_number/code.kt
+++ codefights/kotlin/extra_number/code.kt
@@ -1,6 +1,16 @@
 fun extraNumber(a: Int, b: Int, c: Int): Int {
-    var a = intArrayOf(a, b, c)
-    val grouping = a.groupBy {it}
-    val filtered = grouping.filter {it.value.count() == 1}
-    return filtered.keys.first()
+    val grouping = intArrayOf(a, b, c)
+                   .groupBy {it} // group into frequency 
+                   .filter { it.value.count() == 1 } // get the one which frequency of 1
+    return grouping.keys.first()
+}
+
+fun extraNumber2(a: Int, b: Int, c: Int): Int {
+    var return_number = 0
+    return_number = when {
+        (b == c) -> a
+        (a == c) -> b
+        else -> c
+    }
+    return return_number
 }
