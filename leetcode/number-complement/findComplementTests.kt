d3ff90b7548f31accfb7db129e3956292068c4c0
Muhammad Lukman Low <kianseong@gmail.com>
2018-06-08 22:27:54 +0800

    Adding solution for kotlin number-complement

---
+++ leetcode/number-complement/findComplementTests.kt
@@ -0,0 +1,18 @@
+import org.junit.Test
+
+class ReverseIntegerTest {
+
+    @Test
+    fun testComplement() {
+        val ri = Solution()
+
+        assert(ri.findComplement(5) == 2)
+
+        // assert(ri.reverse(-123) == -321)
+
+        // assert(ri.reverse(1000000003) == 0)
+
+        // assert(ri.reverse(1534236469) == 0)
+    }
+
+}
